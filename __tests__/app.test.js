ee7a7a744af0cf247c6161d749be9dbf133eaa04
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-15 15:36:34 -0700

    findAll working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Flower = require('../lib/models/Flower');
+// const flowers = require('../lib/controllers/flowers');
 
 describe('AnyAPI routes', () => {
   beforeEach(() => {
@@ -19,5 +21,11 @@ describe('AnyAPI routes', () => {
     };
     const res = await request(app).post('/api/v1/flowers').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of flowers', async () => {
+    const expected = await Flower.findAll();
+    const res = await request(app).get('/api/v1/flowers');
+    expect(res.body).toEqual(expected);
   });
 });
